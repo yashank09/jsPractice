c4ede0679328583bdc2d9a1fa0fc1fbf07b7901d
yashank09 <yashank.cronaldo@gmail.com>
2019-06-02 12:44:25 -0700

    Added Functionalities

--- budget_app/js/app.js
+++ budget_app/js/app.js
@@ -45,6 +45,31 @@ class UI {
     return total;
   }
 
+  addExpense(expense) {
+    const div = document.createElement("div");
+    div.classList.add("expense");
+    div.innerHTML = `
+        <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+         <h6 class="expense-title mb-0 text-uppercase list-item">${
+           expense.title
+         }</h6>
+         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+         <div class="expense-icons list-item">
+
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div>
+    `;
+    this.expenseList.appendChild(div);
+  }
+
   //Submit Budget Form
   submitBudgetForm() {
     const budgetValue = this.budgetInput.value;
@@ -95,6 +120,7 @@ class UI {
       //Increment ID and add to Expenses
       this.itemID++;
       this.itemList.push(expense);
+      this.addExpense(expense);
     }
 
     console.log(this.itemList);
